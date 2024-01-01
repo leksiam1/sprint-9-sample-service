0770d308dd84c6b347312ece9cec1d94fa37434b
babintsev <babintsev@banki.ru>
2024-01-02 00:28:31 +0300

    Шапка класса `StgMessageProcessor` готова

--- service_stg/src/stg_loader/stg_message_processor_job.py
+++ service_stg/src/stg_loader/stg_message_processor_job.py
@@ -1,11 +1,23 @@
 import time
 from datetime import datetime
 from logging import Logger
-
+from lib.kafka_connect import KafkaConsumer, KafkaProducer
+from lib.redis import RedisClient
+from stg_loader.repository.stg_repository import StgRepository
 
 class StgMessageProcessor:
     def __init__(self,
+                 consumer: KafkaConsumer,
+                 producer: KafkaProducer,
+                 redis: RedisClient,
+                 stg_repository: StgRepository,
+                 batch_size: int,                 
                  logger: Logger) -> None:
+        self._consumer = consumer
+        self._producer = producer
+        self._redis = redis
+        self._stg_repository = stg_repository
+        self._batch_size = batch_size
         self._logger = logger
 
     # функция, которая будет вызываться по расписанию.
